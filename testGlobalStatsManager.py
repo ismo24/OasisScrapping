558c275e6b53e9d0f1d777e971d719fd1ea40681
ismo24 <ismaelkoura@gmail.com>
2024-07-14 15:50:38 +0000

    correct 4

--- testGlobalStatsManager.py
+++ testGlobalStatsManager.py
@@ -2,10 +2,10 @@ import random
 from GlobalSqlStatsManager import create_global_stat_table,add_new_global_stat_year_model,update_global_stat,update_existing_global_stat_year_model,display_all_global_stats
 
 
-marks = ["Toyota"]
-# , "Honda", "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes", "Audi", "Volkswagen", "Hyundai"
-models = ["ModelA"]
-# , "ModelB", "ModelC", "ModelD", "ModelE", "ModelF", "ModelG", "ModelH", "ModelI", "ModelJ"
+marks = ["Toyota", "Honda"]
+# , "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes", "Audi", "Volkswagen", "Hyundai"
+models = ["ModelA", "ModelB"]
+# , "ModelC", "ModelD", "ModelE", "ModelF", "ModelG", "ModelH", "ModelI", "ModelJ"
 years = [2010, 2011, 2012]
 # , 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
 
@@ -26,14 +26,21 @@ def generate_and_insert_data():
         year = random.choice(years)
         action = random.choice(["clicks", "favorites", "messages", "calls"])
         
+        # infos = {
+        #     "action": action,
+        #     "data": {
+        #         "mark": mark,
+        #         "model": model,
+        #         "year": year
+        #     }
+        # }
         infos = {
-            "action": action,
+            "action":action ,
             "data": {
-                "mark": mark,
-                "model": model,
-                "year": year
-            }
-        }
+                "mark": "BMW",
+                "model": "ModelE",
+                "year": 2012
+            }}
 
         update_global_stat(infos)
 
